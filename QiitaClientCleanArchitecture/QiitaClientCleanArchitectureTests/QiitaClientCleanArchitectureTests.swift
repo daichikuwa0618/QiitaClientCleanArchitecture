fb363761099a3807c9bb7854d38a232b67bb400b
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-07-19 17:36:21 +0900

    add: empty xcode project

---
+++ QiitaClientCleanArchitecture/QiitaClientCleanArchitectureTests/QiitaClientCleanArchitectureTests.swift
@@ -0,0 +1,34 @@
+//
+//  QiitaClientCleanArchitectureTests.swift
+//  QiitaClientCleanArchitectureTests
+//
+//  Created by Daichi Hayashi on 2020/07/19.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import XCTest
+@testable import QiitaClientCleanArchitecture
+
+class QiitaClientCleanArchitectureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
